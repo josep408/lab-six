643562817d1087c1af3456c9494c659afb508fa0
jose <josep2789@icloud.com>
2020-03-23 01:46:22 -0700

    app ready

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+//routes
+app.get("/", function(req,res){
+	res.render("index.html");
+});
+
+app.get("/mercury", function(req,res){
+	res.render("mercury.html")
+});
+
+app.get("/venus", function(req,res){
+	res.render("venus.html")
+});
+
+app.get("/earth", function(req,res){
+	res.render("earth.html")
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
+
+// server Listener
+// app.listen("8081","127.0.0.1", function(){
+// 	console.log("Express server is Running ...");
+// });
+
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("Express server is Running ...");
+});
